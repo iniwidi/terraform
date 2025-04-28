83c94ea45d8118e46b9a4240f7a133fc6de56a04
Widianto <aku@widianto.org>
2025-04-28 17:12:42 +0700

    modified outputs.tf add public_ip, rds_endpoint, s3_bucket_name

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.mysql.endpoint
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.app_bucket.bucket
-}
-widianto@ID-LPT-073:~/terraform$ cat variables.tf
 variable "region" {
   default = "ap-southeast-1"
 }
@@ -20,4 +8,3 @@ variable "db_username" {
 
 variable "db_password" {
   default = "tempAdmin954*"
-}
