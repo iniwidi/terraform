926ab425cfa7e9967931cf2090fe03ed043ac205
Widianto <aku@widianto.org>
2025-05-19 17:22:30 +0700

    adding step install awscli

--- setup.sh
+++ setup.sh
@@ -10,23 +10,25 @@ BINARY_FILE="Binary-linux-x64.7z"
 DDL_FILE="TodoItem_DDL.sql"
 APP_DLL="TodoWebAPI.dll"
 
-RDS_ENDPOINT="terraform-20250429021147512700000001.c5ya80m00geq.ap-southeast-1.rds.amazonaws.com"        # default local MySQL server
+RDS_ENDPOINT="localhost"        # default local MySQL server
 DB_NAME="MyWebApiDB"
 DB_USER="tempAdmin"
 DB_PASS="!tempAdmin954*"
 
+MYSQL_ROOT_PASSWORD="RootPassword123!"
 APP_PORT=5000
 
-# === 0. Copies Code From S3 Bucket ===
-echo "[*] Copies Code..."
-aws s3 cp s3://my-webapp-bucket-91a19527/ ~/ --recursive
+# === install aws cli ===
+sudo apt-get update
+NEEDRESTART_MODE=a sudo apt-get install python3-pip
+sudo pip install awscli
 
 # === 1. Update & Install Dependencies ===
 echo "[*] Updating system..."
-sudo apt update && sudo apt upgrade -y
+NEEDRESTART_MODE=a sudo apt update 
 
 echo "[*] Installing required packages..."
-sudo apt install -y p7zip-full nginx wget
+NEEDRESTART_MODE=a sudo apt install -y p7zip-full nginx wget mysql-server
 
 # Install .NET Runtime
 echo "[*] Installing .NET Runtime..."
@@ -35,10 +37,25 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
 sudo apt install -y aspnetcore-runtime-8.0
 
+# === 2. Configure MySQL ===
+echo "[*] Securing MySQL installation..."
+
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
+
+# Create database and user
+echo "[*] Creating database and user..."
+sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%'; FLUSH PRIVILEGES;"
+
+# Allow remote connection (optional for later)
+sudo sed -i 's/bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
+
 # === 3. Import DDL SQL ===
 if [ -f "$DDL_FILE" ]; then
     echo "[*] Importing DDL file into database..."
-    sudo mysql -u "${DB_USER}" -p"${DB_PASS}" "${DB_NAME}" < "${DDL_FILE}"
+    sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" "${DB_NAME}" < "${DDL_FILE}"
 else
     echo "[!] DDL file '${DDL_FILE}' not found, skipping SQL import!"
 fi
@@ -109,7 +126,8 @@ echo ""
 echo "=========================================="
 echo "✅ Deployment Completed Successfully!"
 echo ""
-echo "🌐 Access your app at: http://[your-server-ip]/api-docs/"
+echo "🌐 Access your app at: http://[your-server-ip]/api-docs"
+echo "🔐 MySQL Root Password: ${MYSQL_ROOT_PASSWORD}"
 echo "🔐 MySQL App User: ${DB_USER} / Password: ${DB_PASS}"
 echo "📂 App Folder: ${APP_DIR}"
 echo "=========================================="
